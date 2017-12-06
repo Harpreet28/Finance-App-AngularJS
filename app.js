d16287e40dd495e90ec561de111e9b03eed6d5c5
Harpreet28 <hkamboj02@gmail.com>
2017-12-05 20:18:58 -0800

    Chages to improve the view

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app', ['ngRoute', 'ngCookies'])
+        .module('app', ['ngRoute', 'ngCookies', 'angularUtils.directives.dirPagination'])
         .config(config)
         .run(run);
 
